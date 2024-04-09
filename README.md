3b8bd7d211be83ad68cfcc59e9131b03cffab80e
omerholz <2319152+omerholz@users.noreply.github.com>
2024-04-09 19:21:22 -0400

    Serverless telegram bot with python and Pulumi  (#1)
    basic-serverless-telegram-chatbot

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # chatbot-example
-An example chatbot demonstrating different tools and technologies 
+An example chatbot demonstrating different tools and technologies.
+
+This repo is accompanied by a blog post that explains how to build a chatbot using different tools and technologies. The blog post is divided into multiple parts. In this part, we will see how to build a simple chatbot using AWS Lambda and Pulumi.
+Check out the blog post [here](https://omerholz.com/blog/serverless-telegram-bot/).
+
+## Prerequisites
+
+- Python 3.12
+- AWS Account
+- [AWS CLI installed and configured](https://github.com/aws/aws-cli)
+- [Pulumi CLI installed and configured](https://www.pulumi.com/docs/install/)
+- Telegram Bot Token
+
+## Setup
+
+Clone the repository and cd into the infra directory.
+
+Set your Telegram Bot Token as a Pulumi secret.
+
+```bash
+pulumi config set --secret telegram_bot_token <YOUR_TELEGRAM_BOT_TOKEN> 
+```
+
+## Deploy
+
+Run the following command to deploy the infrastructure.
+
+```bash
+pulumi up
+```
+
+Pulumi will diagnose the stack and prompt you to confirm the deployment. Type `yes` and hit enter. That's it! Pulumi will deploy the infrastructure and output the URL of the API Gateway. At this point your chatbot is up and running. Try and talk to it!
+
+## Clean up
+
+To clean up the resources, run the following command.
+
+```bash
+pulumi destroy
+```
+
+## Building your own chatbot
+
+You can see how you can easily edit the code in the bot/handler.py file to customize the chatbot. You can add more intents and responses to make the chatbot more intelligent. In the next part of the accounpaning blog post, we will see how to use LLMs to make our bot more chatty and intelligent. Stay tuned!
---
+++ infra/bot_lambda.py
@@ -0,0 +1,105 @@
+import json
+import os
+
+import pulumi
+import pulumi_aws as aws
+from pulumi_aws import iam
+
+from telegram_webhook_provider import Webhook
+from utils import zip_directory, install_dependencies_and_prepare_layer, PY_VER
+
+
+bot_dir = '../bot'
+
+
+def setup_lambda_layer():
+    # Define AWS resources
+    lambda_layer_zip_file = 'target/dependencies_layer.zip'
+    layer_requirements_path = os.path.join(bot_dir, 'requirements.txt')
+
+    # Prepare the Lambda layer package with dependencies
+    output_file_name = install_dependencies_and_prepare_layer(layer_requirements_path, lambda_layer_zip_file)
+
+    zipped_code = pulumi.FileArchive(output_file_name)
+    # Create the Lambda layer
+    lambda_layer = aws.lambda_.LayerVersion("telegramLambdaLayer",
+                                            layer_name="telegram-dependencies-layer",
+                                            code=zipped_code,
+                                            compatible_runtimes=[PY_VER],
+                                            )
+
+    return lambda_layer
+
+def setup_iam_role_for_lambda():
+
+    # Create an IAM role for the Lambda function
+    role = iam.Role("lambdaRole",
+                    assume_role_policy=json.dumps({
+                        "Version": "2012-10-17",
+                        "Statement": [{
+                            "Action": "sts:AssumeRole",
+                            "Principal": {"Service": "lambda.amazonaws.com"},
+                            "Effect": "Allow"
+                        }]
+                    }))
+
+    iam.RolePolicyAttachment("lambdaLogs",
+                             role=role.name,
+                             policy_arn="arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole")
+
+    return role
+
+def setup_lambda_function(lambda_layer, role, token_config_key="telegram_bot_token"):
+
+    # Function code zip (excluding dependencies)
+    zip_file = 'bot_code.zip'
+    zip_directory(bot_dir, zip_file)
+
+    # Create the Lambda function
+    lambda_function = aws.lambda_.Function("TelegramBot",
+                                        handler="handler.lambda_handler",
+                                        role=role.arn,
+                                        runtime=PY_VER,
+                                        code=pulumi.FileArchive(zip_file),
+                                        layers=[lambda_layer.arn],
+                                        environment=aws.lambda_.FunctionEnvironmentArgs(
+                                            variables={
+                                                "TELEGRAM_BOT_TOKEN": pulumi.Config().require_secret("telegram_bot_token")
+                                            }
+                                        ))
+
+    pulumi.export('lambda_function_name', lambda_function.name)
+
+    return lambda_function
+
+
+def setup_api_gateway(lambda_function):
+    # Create the HTTP API Gateway
+    api = aws.apigatewayv2.Api("telegramBotApi",
+        protocol_type="HTTP",
+        route_key="POST /bot",
+        target=lambda_function.invoke_arn)
+
+
+    # Set Lambda permission for API Gateway
+    lambda_permission = aws.lambda_.Permission("ApiGatewayPermission",
+                                               action="lambda:InvokeFunction",
+                                               function=lambda_function.name,
+                                               principal="apigateway.amazonaws.com",
+                                               source_arn=api.execution_arn.apply(lambda arn: f"{arn}/*/*"))
+
+    # Export the API endpoint URL
+    pulumi.export('api_url', api.api_endpoint)
+
+    return api
+
+def register_webhook(api, token_config_key="telegram_bot_token"):
+    # Register the Telegram webhook using the full URL including the route
+    webhook_url = pulumi.Output.concat(api.api_endpoint, "/bot")
+    webhook = Webhook("telegramWebhookRegistration",
+                      token=pulumi.Config().require_secret(token_config_key),
+                      url=webhook_url)
+
+    # Export the API endpoint URL including the /bot route
+    pulumi.export('api_url_with_bot', webhook_url)
+
